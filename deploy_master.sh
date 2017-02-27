20b412f17fa8d7a59e66d54192fe3fdfa83079f9
Karthink <karthink@Karthink-MBPRD.local>
2017-02-27 15:54:41 -0500

    modified files

---
+++ deploy_master.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+ssh kmannem@rcq-usweb01.v2tech.us
+
+mkdir /tmp/karthik_circleci_tests
+
+cd /tmp/karthik_circleci_tests
+
+git clone https://github.com/V2Migration/learn-nightwatch.git
